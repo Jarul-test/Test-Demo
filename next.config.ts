1dc3069d01a5a61fd25a641d1f17bdc4bde3a3d4
jarul-test <jaruldev@gmail.com>
2025-02-13 13:18:55 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: '/Test-Demo', // repo name
 };
 
 export default nextConfig;
